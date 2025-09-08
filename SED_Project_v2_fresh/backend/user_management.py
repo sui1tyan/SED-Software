9b0eed75a25b2de99c63633cbf36a157274f8fde
sui1tyan <ivanngzhy@gmail.com>
2025-09-08 14:37:31 +0800

    Delete SED_Project_v2_fresh directory

--- SED_Project_v2_fresh/backend/user_management.py
+++
@@ -1,6 +0,0 @@
-
-from .login import create_user, delete_user
-def add_user(username, password, role='user'):
-    return create_user(username, password, role)
-def remove_user(requestor_username, username):
-    return delete_user(requestor_username, username)
--- SED_Project_v2_fresh/setup_windows.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-python -m venv venv
-call venv\Scripts\activate
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-python build_exe.py
-echo Build complete. See dist\SED_App_v2.exe
-pause
